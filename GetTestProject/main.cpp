7d00c766abf688a21cd4864b4c627c2788cf2a36
MakoginDenys <denreadmi4@gmail.com>
2021-02-19 19:33:35 +0200

    add argument

--- GetTestProject/main.cpp
+++ GetTestProject/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
-int main()
+int main(int argc,char* argv[])
 {
-	cout << "Hello World!!" << endl;
+	for (size_t i = 0; i < argc; i++)
+	{
+		cout << argv[i] << endl;
+	}
 	return 0;
 }
